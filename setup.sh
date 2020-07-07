90e7ace77465179a3c399ed8a7e32af247c618ba
FiBe4000 <fibeube@gmail.com>
2020-07-07 15:26:18 +0200

    Fix setup script and change from copy to symlink

--- setup.sh
+++ setup.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
+echo "Running FiBe4000 Vim config setup..."
+
+# Date used for backup name
 date=$(date +"%d-%m-%Y")
 
+# Path to vim config
+vimconfpath=`pwd`
+
 # Find the user running the script
-if [ $SUDO_USER ]; then
-    user=$SUDO_USER
-else
-    user=`whoami`
-fi
+user=`logname`
 
 # Backup current .vimrc if there is one
 if [ -e /home/$user/.vimrc ]; then
-    sudo -u mv /home/$user/.vimrc /home/$user/.vimrc.$date
+    sudo -u $user mv /home/$user/.vimrc /home/$user/.vimrc.$date
 fi
 
 # Use the new .vimrc
-sudo -u $user cp .vimrc /home/$user/.vimrc
+sudo -u $user ln -sfn $vimconfpath/vimrc /home/$user/.vimrc
 
 # Create the directory for persistend undo
 sudo -u $user mkdir -p /home/$user/.vim/undodir
@@ -24,7 +26,7 @@ sudo -u $user mkdir -p /home/$user/.vim/undodir
 sudo -u $user mkdir -p /home/$user/.vim/after/syntax
 
 # Install needed packages
-pacman -S --noconfirm git python-neovim ctags clang wget
+pacman -S --noconfirm git python-neovim ctags clang
 
 # Download Vundle for plugin management
 sudo -u $user git clone https://github.com/VundleVim/Vundle.vim.git /home/$user/.vim/bundle/Vundle.vim
@@ -37,12 +39,4 @@ sudo -u $user rm -rf c-syntax.vim
 # Install the plugins specified in .vimrc
 sudo -u $user vim +PluginInstall +qall
 
-if [ $? -eq 0 ]; then
-    sudo -u $user chown -R $user:$user home/$user/.vim
-    sudo -u $user chown -R $user:$user home/$user/.vimrc
-    sudo -u $user chown -R $user:$user home/$user/.vimrc.$date
-    echo "Done"
-else
-    echo "Error! See output for more information."
-fi
-
+echo "FiBe4000 Vim config installed."
